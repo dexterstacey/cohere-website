d6235e8f120b1f22a9d71eb9250f46d3527f771a
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-05-13 14:35:11 +0300

    Added reports page + data-src works with parcel

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -20,8 +20,9 @@ class Navbar extends HTMLElement {
                       Menu
                   </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item u-text-mid" href="/./aboutus.html">About Us</a></li>
-                <li><a class="dropdown-item u-text-mid" href="/./getinvolved.html">Get Involved</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./aboutus.html">About Us</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./getinvolved.html">Get Involved</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./reports.html">Reports</a></li>
                 <li><a class="dropdown-item u-text-mid" href="https://news.wearecohere.org">News</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item u-text-mid" href="./index.html">Home</a></li>
