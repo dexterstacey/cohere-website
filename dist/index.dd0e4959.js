c1c7303273b32d796bb3a0b6d524b38db90bcd60
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2022-08-05 09:13:58 +0300

    Pulled changes & changed footer text

--- dist/index.dd0e4959.js
+++ dist/index.dd0e4959.js
@@ -553,9 +553,9 @@ class Navbar extends HTMLElement {
                       Menu
                   </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item u-text-mid" href="./aboutus.html">About Us</a></li>
-                <li><a class="dropdown-item u-text-mid" href="./getinvolved.html">Get Involved</a></li>
-                <li><a class="dropdown-item u-text-mid" href="./reports.html">Reports</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./aboutus">About Us</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./getinvolved">Get Involved</a></li>
+                <li><a class="dropdown-item u-text-mid" href="./reports">Reports</a></li>
                 <li><a class="dropdown-item u-text-mid" href="./team">Team</a></li>
                 <li><a class="dropdown-item u-text-mid" href="https://news.wearecohere.org">News</a></li>
                 <li><hr class="dropdown-divider"></li>
